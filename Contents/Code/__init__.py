fe5ff9d6bb86222afb10c5e112c378eb7134a1f0
sander1 <sanderspies+plexapp@gmail.com>
2011-06-09 00:23:08 +0200

    Fix Apple trailers, remove some duplicate functions

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,102 +1,88 @@
+# Icon and Code by Ryan McNally. Art by jpmatth on flicker http://www.flickr.com/photos/jpmatth/979337931/
 import string
 
-PLUGIN_PREFIX = "/video/HDTrailers"
-TOP_10 = "http://www.hd-trailers.net/TopMovies/"
-LATEST = "http://www.hd-trailers.net/Page/"
-LIBRARY = "http://www.hd-trailers.net/PosterLibrary/"
-OPENING = "http://www.hd-trailers.net/OpeningThisWeek/"
-COMING_SOON = "http://www.hd-trailers.net/ComingSoon/"
-BLU_RAY = "http://www.hd-trailers.net/BluRay/"
-BASE = "http://www.hd-trailers.net"
-
-CACHE_TIME = 3600
-
-
-# art-default from:
-#<div xmlns:cc="http://creativecommons.org/ns#" about="http://www.flickr.com/photos/jpmatth/979337931/"><a rel="cc:attributionURL" href="http://www.flickr.com/photos/jpmatth/">http://www.flickr.com/photos/jpmatth/</a> / <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/2.0/">CC BY-NC-ND 2.0</a></div>
-# settings icon from Oxygen
+BASE = 'http://www.hd-trailers.net'
+TOP_10 = '%s/TopMovies/' % BASE
+LATEST = '%s/Page/%%d' % BASE
+LIBRARY = '%s/PosterLibrary/%%s' % BASE
+OPENING = '%s/OpeningThisWeek/' % BASE
+COMING_SOON = '%s/ComingSoon/' % BASE
+BLU_RAY = '%s/BluRay/' % BASE
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "HD Trailers.net", "icon-default.png", "art-default.jpg")
-  MediaContainer.title1 = L('HD Trailers.net')
-  MediaContainer.art = R('art-default.jpg')
-  DirectoryItem.thumb = R("icon-default.png")
-  HTTP.CacheTime = CACHE_TIME
-####################################################################################################
+  Plugin.AddPrefixHandler('/video/hdtrailers', MainMenu, 'HD Trailers.net', ICON, ART)
+  Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
 
+  MediaContainer.art = R(ART)
+  MediaContainer.title1 = 'HD Trailers.net'
+  MediaContainer.userAgent = 'Apple Mac OS X v10.6.7 CoreMedia v1.0.0.10J869'
+  MediaContainer.viewGroup = 'List'
+  DirectoryItem.thumb = R(ICON)
+
+  HTTP.CacheTime = CACHE_1HOUR
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'
+
+####################################################################################################
 def MainMenu():
   dir = MediaContainer()
-  dir.Append(Function(DirectoryItem(TopTen, "Top 10")))
-  dir.Append(Function(DirectoryItem(Latest, "Latest")))
-  dir.Append(Function(DirectoryItem(Opening, "Opening")))
-  dir.Append(Function(DirectoryItem(ComingSoon, "Coming Soon")))
-  dir.Append(Function(DirectoryItem(BluRay, "Blu-Ray")))
-  dir.Append(Function(DirectoryItem(Library, "Library")))
-  dir.Append(PrefsItem("Preferences...", thumb=R('icon-prefs.png')))
-  dir.Append(Function(DirectoryItem(about, 'About', thumb=R('icon-about.png'))))
+  dir.Append(Function(DirectoryItem(Videos, title='Top 10'), url=TOP_10))
+  dir.Append(Function(DirectoryItem(Latest, title='Latest')))
+  dir.Append(Function(DirectoryItem(Videos, title='Opening'), url=OPENING))
+  dir.Append(Function(DirectoryItem(Videos, title='Coming Soon'), url=COMING_SOON))
+  dir.Append(Function(DirectoryItem(Videos, title='Blu-Ray'), url=BLU_RAY))
+  dir.Append(Function(DirectoryItem(Library, title='Library')))
+  dir.Append(PrefsItem(title='Preferences...', thumb=R('icon-prefs.png')))
   return dir
-  
-def TopTen(sender):
-  return Videos(sender, TOP_10)
-  
-def Opening(sender):
-  return Videos(sender, OPENING)
-  
-def ComingSoon(sender):
-  return Videos(sender, COMING_SOON)
-  
-def BluRay(sender):
-  return Videos(sender, BLU_RAY)
-  
+
+####################################################################################################
 def Latest(sender, page=1):
-  dir = Videos(sender, LATEST + str(page))
-  dir.Append(Function(DirectoryItem(Latest, "More ..."), page=page+1))
+  dir = Videos(sender, url=LATEST % page)
+  dir.Append(Function(DirectoryItem(Latest, title='More...'), page=page+1))
   return dir
-  
+
+####################################################################################################
 def Library(sender):
-  dir = MediaContainer()
+  dir = MediaContainer(title=sender.itemTitle)
   for page in list(string.uppercase):
-      dir.Append(Function(DirectoryItem(Videos, page), pageUrl=LIBRARY+page))
+    dir.Append(Function(DirectoryItem(Videos, title=page), url=LIBRARY % page))
   return dir
-  
-def Videos(sender, pageUrl):
-  dir = MediaContainer()
-  for poster in HTML.ElementFromURL(pageUrl).xpath('//td[@class="indexTableTrailerImage"]/a'):
+
+####################################################################################################
+def Videos(sender, url):
+  dir = MediaContainer(title=sender.itemTitle)
+  for poster in HTML.ElementFromURL(url).xpath('//td[@class="indexTableTrailerImage"]/a'):
     if len(poster.xpath('./img')) > 0:
-       url = BASE + poster.get('href')
-       thumb = poster.xpath('./img')[0].get('src')
-       title = poster.xpath('./img')[0].get('alt')
-       dir.Append(Function(PopupDirectoryItem(VideosMenu, title=title, thumb=thumb), url=url))
-  dir.Append(Function(DirectoryItem(about, 'About', thumb=R('icon-about.png'))))
+      url = BASE + poster.get('href')
+      thumb = poster.xpath('./img')[0].get('src')
+      title = poster.xpath('./img')[0].get('alt')
+      dir.Append(Function(PopupDirectoryItem(VideosMenu, title=title, thumb=Function(GetThumb, url=thumb)), url=url))
   return dir
 
-
-def about(sender):
-  return MessageContainer(header='About', message='Icon and Code by Ryan McNally. Art by jpmatth on flicker http://www.flickr.com/photos/jpmatth/979337931/', title1='title1', title2='title2')
-
+####################################################################################################
 def VideosMenu(sender, url):
   dir = MediaContainer(title2=sender.itemTitle)
   defaultRes = Prefs['resolution']
-  content = HTML.ElementFromURL(url)
-  for row in content.xpath('//table[@class="bottomTable"]//tr'):
-	      baseTitleItems = row.xpath('./td[@class="bottomTableName"]')
-	      baseTitle = None
-	      if len(baseTitleItems) > 0:
-	      	baseTitle = baseTitleItems[0].xpath('./span')[0].text
-	      Log(baseTitle)
-	      if baseTitle != None:
-	        for res in row.xpath('./td[@class="bottomTableResolution"]/a'):
-	           resTitle = res.text
-	           videoUrl = res.get('href')
-	           include = True
-	           if videoUrl.startswith("http://movies.apple.com") and Prefs["exclude.apple"]:
-	              include = False
-	           if videoUrl.startswith("http://trailers.apple.com") and Prefs["exclude.apple"]:
-	              include = False
-	           if include and (defaultRes == 'Prompt' or defaultRes == resTitle):
-	              Log("Video URL:"+resTitle+":"+videoUrl)
-	              dir.Append(VideoItem(videoUrl, title='%s %s' % (baseTitle, resTitle)))
-          	   
+
+  for row in HTML.ElementFromURL(url).xpath('//table[@class="bottomTable"]//tr'):
+    baseTitleItems = row.xpath('./td[@class="bottomTableName"]')
+    baseTitle = None
+    if len(baseTitleItems) > 0:
+      baseTitle = baseTitleItems[0].xpath('./span')[0].text
+    if baseTitle is not None:
+      for res in row.xpath('./td[@class="bottomTableResolution"]/a'):
+        resTitle = res.text
+        videoUrl = res.get('href')
+        if defaultRes == 'Prompt' or defaultRes == resTitle:
+          dir.Append(VideoItem(videoUrl, title='%s %s' % (baseTitle, resTitle)))
   return dir
 
+####################################################################################################
+def GetThumb(url):
+  try:
+    data = HTTP.Request(url, cacheTime=CACHE_1MONTH)
+    return DataObject(data, 'image/jpeg')
+  except:
+    return Redirect(R(ICON))
